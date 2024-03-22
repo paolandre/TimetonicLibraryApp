74a01413cd95373059d7a1649eb59782152e6e78
Paola Andrea Ramirez <129551206+paolandre@users.noreply.github.com>
2024-03-22 13:21:08 -0500

    Setup basic UI layout for landing page and resolve dependencies
    Configured the basic layout for activity_landing.xml and item_book.xml to set up the UI for the book listing. Also resolved issues with Gradle dependencies to ensure proper project build.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -61,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
